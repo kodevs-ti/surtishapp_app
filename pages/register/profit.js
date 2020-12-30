265847c22cedf633cf14709e0a3b31752833a7c0
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2020-12-29 20:21:55 -0600

    [UPDATE] react-hook-form profit screen to register product 03

--- pages/register/profit.js
+++ pages/register/profit.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import Router, { useRouter } from 'next/router'
-import { useFormik } from 'formik'
+import { useForm } from 'react-hook-form'
 import * as Yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 
 import ProductWrapper from '../../components/layout/productWrapper'
 import Warning from '../../components/Warning'
@@ -12,8 +13,14 @@ import { updateById } from '../../services/products'
 import piece from '../../public/icon/piece-one.svg'
 import kilo from '../../public/icon/kilo-icon-one.svg'
 
+const schema = Yup.object().shape({
+  pricePurchasedByUnit: Yup.number().min(1, 'Requerido').positive('Invalido').required('Requerido').typeError('Invalido'),
+  percentageOfProfit: Yup.number().min(1, 'Requerido').max(100, 'Invalido').positive('Invalido').required('Requerido').typeError('Invalido')
+})
+
 export default function Stock () {
   const [priceSuggestedByUnit, setPriceSuggestedByUnit] = useState(0)
+  const [quantityOfProfit, setQuantityOfProfit] = useState(0)
   const router = useRouter()
   const {
     barcode,
@@ -21,42 +28,43 @@ export default function Stock () {
     unitMeasureMajor
   } = router.query
 
-  const formik = useFormik({
-    initialValues: {
-      pricePurchasedByUnit: 0,
-      percentageOfProfit: 0
-    },
-    validationSchema: Yup.object({
-      pricePurchasedByUnit: Yup.number().min(1, 'Requerido').positive('Invalido').required('Requerido'),
-      percentageOfProfit: Yup.number().min(1, 'Requerido').max(100).positive('Invalido').required('Requerido')
-    }),
-    onSubmit: async values => {
-      console.log(values)
-      const newData = { ...values, priceSuggestedByUnit }
-      const token = getToken()
-      const response = await updateById(id, newData, token)
-      const responseJSON = await response.json()
-      console.log(responseJSON)
-      const { success } = responseJSON
-      if (success) {
-        Router.push('/register/success')
-      }
-    }
+  const { register, handleSubmit, errors, watch } = useForm({
+    mode: 'onChange',
+    reValidateMode: 'onBlur',
+    resolver: yupResolver(schema)
   })
 
+  const watchPurchases = watch()
+
+  const onSubmit = async (dataToSend) => {
+    console.log(dataToSend)
+    const newData = { ...dataToSend, priceSuggestedByUnit, quantityOfProfit }
+    const token = getToken()
+    const response = await updateById(id, newData, token)
+    const responseJSON = await response.json()
+    console.log(responseJSON)
+    const { success } = responseJSON
+    if (success) {
+      Router.push('/register/success')
+    }
+  }
+
   const priceToSum = (pricePurchasedByUnit, percentageOfProfit) => {
-    const value = (percentageOfProfit * pricePurchasedByUnit) / 100
-    return value
+    const value = (parseFloat(percentageOfProfit) * parseFloat(pricePurchasedByUnit)) / 100
+    return  Math.round(value)
   }
 
   useEffect(() => {
-    setPriceSuggestedByUnit(formik.values.pricePurchasedByUnit + priceToSum(formik.values.pricePurchasedByUnit, formik.values.percentageOfProfit))
-  },
-  [formik.values.percentageOfProfit])
+    setPriceSuggestedByUnit(parseFloat(watchPurchases.pricePurchasedByUnit) + priceToSum(watchPurchases.pricePurchasedByUnit, watchPurchases.percentageOfProfit))
+    setQuantityOfProfit(priceToSum(watchPurchases.pricePurchasedByUnit, watchPurchases.percentageOfProfit))
+  },[watchPurchases.percentageOfProfit])
 
+
+  const classNamePricePurchased = errors.pricePurchasedByUnit ? 'inputErrorPricePurchasedByUnit' : null
+  const classNamePercentageOfProfit = errors.percentageOfProfit ? 'inputErrorPercentageOfProfit' : null
   return (
     <ProductWrapper barcode={barcode}>
-      <form className='form-product' onSubmit={formik.handleSubmit}>
+      <form className='form-product' onSubmit={handleSubmit(onSubmit)}>
         <h3 className='title-style mt-3'>Ganancias</h3>
         <hr />
         <p className='title-style-sec'>Ganancia por cada</p>
@@ -84,7 +92,6 @@ export default function Stock () {
               <Warning content='Recuerda calcular tus ganancias por cada KILO' />
             )
           }
-
         </div>
 
         <div className='d-flex mt-4'>
@@ -93,24 +100,26 @@ export default function Stock () {
             <input
               type='number'
               name='pricePurchasedByUnit'
-              className='form-control input-style'
+              className={`form-control input-style ${classNamePricePurchased}`}
               placeholder='0'
-              value={formik.values.pricePurchasedByUnit}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
+              ref={register}
             />
+            <div className='text-alert-input'>
+              <p>{errors.pricePurchasedByUnit?.message}</p>
+            </div>
           </div>
           <div className='form-group mr-2'>
             <label className='label-style'>% de Ganancia</label>
             <input
               type='number'
               name='percentageOfProfit'
-              className='form-control input-style'
+              className={`form-control input-style ${classNamePercentageOfProfit}`}
               placeholder='0'
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.percentageOfProfit}
+              ref={register}
             />
+            <div className='text-alert-input'>
+              <p>{errors.percentageOfProfit?.message}</p>
+            </div>
           </div>
           <div className='form-group'>
             <label className='label-style'>Precio Sug.</label>
@@ -120,15 +129,15 @@ export default function Stock () {
               className='form-control input-style-read'
               placeholder='0'
               readOnly
-              value={formik.values.pricePurchasedByUnit + priceToSum(formik.values.pricePurchasedByUnit, formik.values.percentageOfProfit)}
+              value={watchPurchases.pricePurchasedByUnit ? parseFloat(watchPurchases.pricePurchasedByUnit) + priceToSum(watchPurchases.pricePurchasedByUnit, watchPurchases.percentageOfProfit) : 0}
             />
           </div>
         </div>
         <div className='wrapper-profit mb-2'>
-          <p>Ganancia de <span>${priceToSum(formik.values.pricePurchasedByUnit, formik.values.percentageOfProfit)}</span></p>
+          <p>Ganancia de <span>${priceToSum(watchPurchases.pricePurchasedByUnit, watchPurchases.percentageOfProfit) ? priceToSum(watchPurchases.pricePurchasedByUnit, watchPurchases.percentageOfProfit) : null}</span></p>
           <p>por cada {unitMeasureMajor === 'caja' ? (<span>pieza</span>) : (<span>KG</span>)} </p>
         </div>
-        <button className='btn-gradient mt-2 mb-5'>
+        <button type='submit' className='btn-gradient mt-2 mb-5'>
           Finalizar <i className='fas fa-arrow-right ml-2' />
         </button>
       </form>
